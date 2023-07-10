b0cf4ac618090254005a4bafcec1b2046502aebf
Liam <liam.staras@gmail.com>
2023-07-10 18:29:01 +0200

    more modification for data splitting

--- data/split.py
+++ data/split.py
@@ -6,21 +6,21 @@ import math
 
 import argparse
 
-CATEGORIES = ['train','validate','test']
+CATEGORIES = ['train','test']
 
-def ratio3(string):
+def ratio2(string):
     input = tuple(int(i) for i in string.split(','))
-    if len(input) != 3:
-        raise TypeError('ratio must be a comma-separated list of length 3')
+    if len(input) != 2:
+        raise TypeError('ratio must be a comma-separated list of length 2')
     output = tuple(i/sum(input) for i in input)
     return output
     
 
-parser = argparse.ArgumentParser(description='split data for training, validation and testing')
+parser = argparse.ArgumentParser(description='split data for training and testing')
 parser.add_argument('path', type=str,
                     help='path to the data')
-parser.add_argument('--ratio', dest='ratio', metavar='train,validate,test', default='80,10,10',
-                    help='ratio to divide (default: "80,10,10")', type=ratio3)
+parser.add_argument('--ratio', dest='ratio', metavar='train,test', default='90,10',
+                    help='ratio to divide (default: "90,10")', type=ratio2)
 
 args = parser.parse_args()
 
@@ -28,5 +28,5 @@ data = np.load(args.path, mmap_mode='r')
 
 splits = [0] + list(math.ceil(elt) for elt in np.cumsum(args.ratio)*data.shape[0])
 
-for i in range(3):
+for i in range(2):
     np.save(str(Path(args.path).with_suffix('')) + '-%s.split' % CATEGORIES[i], range(splits[i],splits[i+1]))
