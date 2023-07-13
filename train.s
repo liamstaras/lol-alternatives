35eea77b81302d1f073c522d30d47d862e447d3b
Liam <liam.staras@gmail.com>
2023-07-13 17:46:09 +0200

    updates to training parameters

--- train.s
+++ train.s
@@ -2,14 +2,14 @@
 #SBATCH --job-name train-lol-palette            # this is a parameter to help you sort your job when listing it
 #SBATCH --ntasks 1                    # number of tasks in your job. One by default
 #SBATCH --cpus-per-task 1             # number of cpus for each task. One by default
-#SBATCH --mem-per-cpu 10000
+#SBATCH --mem-per-cpu 5000
 #SBATCH --partition shared-gpu         # the partition to use. By default debug-cpu
-#SBATCH --time 00:01:00                  # maximum run time.
+#SBATCH --time 03:00:00                  # maximum run time.
 #SBATCH --gpus ampere:1
-##SBATCH --gres VramPerGpu:20G
+#SBATCH --gres VramPerGpu:20G
 
 source ./requirements/modules.sh
 source .env/bin/activate
 echo SLURM allocated GPUs: $CUDA_VISIBLE_DEVICES
-python -m palette train --debug -c configurations/palette/lol.json -gpu $CUDA_VISIBLE_DEVICES
+python -m palette train -c configurations/palette/lol.json -gpu $CUDA_VISIBLE_DEVICES
 deactivate
