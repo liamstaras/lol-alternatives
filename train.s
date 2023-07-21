c4ef0110033e523f4e8dcc9178430017a660e516
Liam <liam.staras@gmail.com>
2023-07-21 18:12:21 +0200

    fixes

--- train.s
+++ train.s
@@ -3,7 +3,8 @@
 #SBATCH --ntasks 1                    # number of tasks in your job. One by default
 #SBATCH --cpus-per-task 1             # number of cpus for each task. One by default
 #SBATCH --mem-per-cpu 10000
-#SBATCH --partition shared-gpu         # the partition to use. By default debug-cpu
+##SBATCH --partition shared-gpu         # the partition to use. By default debug-cpu
+#SBATCH --partition private-dpt-gpu
 #SBATCH --time 01:00:00                  # maximum run time.
 #SBATCH --gpus ampere:1
 ##SBATCH --gres VramPerGpu:40G
@@ -11,5 +12,5 @@
 source ./requirements/modules.sh
 source .env/bin/activate
 echo SLURM allocated GPUs: $CUDA_VISIBLE_DEVICES
-python -m palette train -c configurations/palette/lol.json -gpu $CUDA_VISIBLE_DEVICES
+python -m palette train --debug -c configurations/palette/lol.json -gpu $CUDA_VISIBLE_DEVICES
 deactivate
