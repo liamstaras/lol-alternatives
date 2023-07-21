82d2340790be1dd40a576d9f863ea3c24b540421
Liam <liam.staras@gmail.com>
2023-07-21 18:30:32 +0200

    fix

--- resources/metrics.py
+++ resources/metrics.py
@@ -5,8 +5,8 @@ import torch
 
 def power_spectrum_diff(output, target):
     
-    output_ps, output_k = pb.get_power(output.cpu, 1000)
-    target_ps, target_k = pb.get_power(target.cpu, 1000)
+    output_ps, output_k = pb.get_power(output.cpu(), 1000)
+    target_ps, target_k = pb.get_power(target.cpu(), 1000)
 
     diff = (output_ps - target_ps)/target_ps
     rms = torch.sqrt(torch.mean(diff**2))
