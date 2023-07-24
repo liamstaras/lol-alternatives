a7951e13e3943d14a0e312dbc0bc700f5ac77c93
Liam <liam.staras@gmail.com>
2023-07-24 18:14:55 +0200

    upgrade pip on venv creation

--- mkvenv.sh
+++ mkvenv.sh
@@ -5,6 +5,7 @@ echo Creating virtual environment using requirements.txt...
 source ./requirements/modules.sh
 python -m venv .env
 source .env/bin/activate
+pip install --upgrade pip
 pip install -r ./requirements/pip.txt
 deactivate
 module purge
