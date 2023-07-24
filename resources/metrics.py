94ba0e190a7e769d32013f54f1e270e28a2d673a
Liam <liam.staras@gmail.com>
2023-07-24 16:37:35 +0200

    split configuration for training on log versus raw

--- resources/metrics.py
+++ resources/metrics.py
@@ -4,9 +4,14 @@ import powerbox as pb
 import numpy as np
 
 def power_spectrum_diff(output, target):
-    
-    output_ps, output_k = pb.get_power(output.cpu(), 1000)
-    target_ps, target_k = pb.get_power(target.cpu(), 1000)
+    return _power_spectrum_diff(output.cpu(), target.cpu())
+
+def power_spectrum_diff_log(output, target):
+    return _power_spectrum_diff(np.exp(output.cpu())-1, np.exp(target.cpu())-1)
+
+def _power_spectrum_diff(output, target):
+    output_ps, output_k = pb.get_power(output, 1000)
+    target_ps, target_k = pb.get_power(target, 1000)
 
     diff = (output_ps - target_ps)/target_ps
     rms = np.sqrt(np.mean(diff**2))
--- train-log.s
+++ train-log.s
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name train-lol-palette            # this is a parameter to help you sort your job when listing it
+#SBATCH --job-name train-lol-log-palette            # this is a parameter to help you sort your job when listing it
 #SBATCH --ntasks 1                    # number of tasks in your job. One by default
 #SBATCH --cpus-per-task 1             # number of cpus for each task. One by default
 #SBATCH --mem-per-cpu 10000
@@ -11,5 +11,5 @@
 source ./requirements/modules.sh
 source .env/bin/activate
 echo SLURM allocated GPUs: $CUDA_VISIBLE_DEVICES
-python -m palette train -c configurations/palette/lol.json -gpu $CUDA_VISIBLE_DEVICES
+python -m palette train -c configurations/palette/lol-log.json -gpu $CUDA_VISIBLE_DEVICES
 deactivate
